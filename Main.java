594ce5cbf44e7c71d92e7d96144a12487997a767
korks2931 <71141021+korks2931@users.noreply.github.com>
2020-10-06 20:31:37 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *This program will show the pattern of the number.
+ * @author Sevde
  */
 public class Main {
 
@@ -9,7 +10,51 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a Scanner for user input
+    Scanner input = new Scanner(System.in);
     
+    // ask for a positive integer 
+    System.out.println("Please enter a positive integer");
+    // declare a variable to store userInteger
+    int n = input.nextInt();
+    int count = n;
+   
+  
+   while( count != 1){
+   int number =  count % 2;
+    if ( number == 0){
+     count = (count / 2);
+    System.out.println(count);
+    }
+   
+    if ( number > 0){
+     count = (3 * count + 1);
+    System.out.println(count);
+    }
+    }
+   
+    }
   }
-}
+
+  
+
+      
+      
+    
+       
+      
+        
+        
+  
+
+       
+       
+
+        
+      
+
+    
+      
+  
+  
+
